9517ef699c9d26152bb10a83213a7ce76d74daba
Jacob Tye <jacob@eminenceaus.com>
2022-05-14 15:13:36 +1000

    Feat: Parts List

--- pages/sets/[id].tsx
+++ pages/sets/[id].tsx
@@ -3,12 +3,15 @@ import Head from "next/head";
 import Link from "next/link";
 import Jumbotron from "../../components/jumbotron/jumbotron";
 import { getAllSetIds, getSetsData } from "../../lib/sets";
+import { getSetPartsList } from "../../lib/parts";
 
 export async function getStaticProps({ params }: any) {
   const setData = await getSetsData(JSON.stringify(params.id));
+  const partsList = await getSetPartsList(JSON.stringify(params.id));
   return {
     props: {
       setData,
+      partsList,
     },
   };
 }
@@ -21,13 +24,13 @@ export async function getStaticPaths() {
   };
 }
 
-export default function Set({ setData }: any) {
+export default function Set({ setData, partsList }: any) {
   const set = {
     title: setData.name,
     content:
       "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.",
     linkLabel: "Buy Set",
-    linkUrl: '#',
+    linkUrl: "#",
     bgColor: "bg-light",
     txtColor: "dark",
   };
@@ -56,6 +59,28 @@ export default function Set({ setData }: any) {
         <div className="col-md-12">
           <Jumbotron {...set}></Jumbotron>
         </div>
+      </div>
+      <div className="row my-5">
+        {partsList.length > 1 && <h2>Parts List for {setData.name}</h2>}
+        <table className="table">
+          <thead>
+            <tr>
+              <th scope="col">Part No.</th>
+              <th scope="col">Part Color</th>
+              <th scope="col">Quantity</th>
+            </tr>
+          </thead>
+          <tbody>
+            {partsList.length > 1 &&
+              partsList.map((item: any) => (
+                <tr>
+                  <th scope="row">{item.part_num}</th>
+                  <td>{item.color_id}</td>
+                  <td>{item.quantity}</td>
+                </tr>
+              ))}
+          </tbody>
+        </table>
       </div>
     </Layout>
   );
